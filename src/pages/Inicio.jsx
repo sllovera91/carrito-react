6df2be11813bf57598f0e683e0c75f8b3eeefcbd
Santiago Llovera <105535098+sllovera91@users.noreply.github.com>
2022-10-17 23:34:35 -0500

    Add files via upload

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,5 +1,4 @@
-import { Container, Grid, Box } from "@mui/material"
-import { Producto } from "../components/Producto"
+import { Container, Grid, Box, Button } from "@mui/material"
 
 import { useEffect, useState } from "react"
 import { totalProductos } from "../helpers/useAxios"
@@ -19,9 +18,19 @@ export const Inicio = () => {
         <Grid container spacing={5}>
         { productos != null ? 
           productos.map( (product) => (
-            <Producto key={product}>
-            </Producto>
-          ))
+            <Grid item xs={12} sm={3} key={product}>
+            <Box sx={{ border: 1 }}>
+              <h3>{product.title}</h3>
+              <img src={product.image} alt=""/>
+              <p>USD-{product.price}</p>
+              <div className="Boton">
+                <Button   size="small"> <i className="fa-solid fa-plus"></i> </Button>
+                <p>Cantidad: 0</p>
+                <Button   size="small"> <i className="fa-solid fa-minus"></i> </Button>
+              </div>
+            </Box>
+            </Grid>
+          ))         
           : ('Cargando')}
         </Grid>
       </Container>
