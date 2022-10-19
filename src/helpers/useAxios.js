15ac355ce295a1acfd86fee2a8acc21ba3cd6ab8
Santiago Llovera <105535098+sllovera91@users.noreply.github.com>
2022-10-19 13:30:00 -0300

    Delete src/helpers directory

--- src/helpers/useAxios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const totalProductos = async (state) => {
-    const peticion = await axios.get('https://fakestoreapi.com/products')
-    state(peticion.data)
-}
